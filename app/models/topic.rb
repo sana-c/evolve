b4c3e6e00ca05765b6f7a837f71eea3df59bfeae
polo Garniga <kenpococo@gmail.com>
2023-03-08 11:09:31 +0100

    Topic Show Page
    Signed-off-by: polo Garniga <kenpococo@gmail.com>

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,7 +1,9 @@
 class Topic < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many :tags, through: :topic_tag
   has_many :reviews
   has_many :reservations
+  has_many :topic_tags
+  has_many :tags, through: :topic_tags
+
 end
