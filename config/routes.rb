c2b329b86f7b9cfa56d8edd415bc776223850b89
khalidltf <khalidlotfi2005@gmail.com>
2023-03-09 17:32:16 +0100

    working on reservation calendar + reseravtion show
    Signed-off-by: khalidltf <khalidlotfi2005@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
+  get 'reservations/create'
   root to: "pages#home"
   devise_for :users
-  
-  resources :users do
-    resources :topics, only: [:create, :new, :show]
-  end
 
-  resources :topics, only: [:index]
+  resources :users do
+    resources :topics, only: [:create, :new]
+  end
+  resources :topics do
+    resources :reservations, only: [:create, :new]
+  end
+  resources :reservations, only: [:show]
+
+  resources :topics, only: [:index, :show]
 
   get "/discover", to: "pages#discover", as: "discover"
 end
