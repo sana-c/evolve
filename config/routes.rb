e8980182ad3d228fdc8acd69d655b1aa8561a3cb
sanaa semlali <sanaa.semlali@gmail.com>
2023-03-09 11:05:13 +0100

    lines deleted

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,10 @@ Rails.application.routes.draw do
   get 'categories/index'
   get 'categories/show'
   devise_for :users
-  resources :users do 
-    resources :topics, only: [:index, :show, :create, :new] 
+  resources :users do
+    resources :topics, only: [:index, :show, :create, :new]
   end
-  
   root to: "pages#home"
-
-   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
