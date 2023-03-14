e98e7d2ab2ba0cb36a3ad6851237dd43cce6734f
sanaa semlali <sanaa.semlali@gmail.com>
2023-03-14 18:43:46 +0100

    seed and check reviews
    Signed-off-by: sanaa semlali <sanaa.semlali@gmail.com>

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,7 @@ class Topic < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :reviews
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   has_many :topic_tags
   has_many :tags, through: :topic_tags
   has_one_attached :photo
