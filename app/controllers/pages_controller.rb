a010fc2d71ccafc8c9ef4ead527f857d29e89f68
sanaa semlali <sanaa.semlali@gmail.com>
2023-03-16 11:44:34 +0100

    discover refixed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,20 +8,19 @@ class PagesController < ApplicationController
 
   def discover
     @categories = Category.all
-
     if params[:q].present?
-
-      @target_category = Category.where(["name = ? ", "#{params[:q]}"])
-      @topics = Topic.where(["category_id = ? ", "#{@target_category.ids.first}"])
-
+      @target_category = Category.where("name ILIKE ?",  "%#{params[:q]}%").first
+      @topics = @target_category ? @target_category.topics : Topic.all
     else
       @topics = Topic.all
     end
+
+    if !params[:q].present?
+      if params[:query].present?
+        @topics = Topic.where("title ILIKE ?", "%#{params[:query]}%")
+      else
+        @topics = Topic.all
+      end
+    end
   end
-
-
-  def sucess
-
-  end
-  
 end
